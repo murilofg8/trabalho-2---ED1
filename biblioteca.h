3e210585eec87c4b5b74b2253116dac56319c3fe
thmarcarini <thiagofabriciorm@gmail.com>
2026-06-25 14:19:52 -0300

    Add data and livro structs to biblioteca.h

--- biblioteca.h
+++ biblioteca.h
@@ -2,5 +2,24 @@
 #define _ESTOQUE_H_
 
 #include "AVL.h"
+#include <string.h>
+
+typedef struct data{
+     int dia, mes, ano;
+}data;
+
+typedef struct livro{
+     char titulo[200];
+     char autor [200];
+     char editora[50];
+     int  lancamento;
+     int  ed;
+     int status;
+     //historico de emprestivo: usar lista ou arvore;
+}livro;
+
+int ordenaLivro(livro a, livro b);
+
+
 
 #endif
