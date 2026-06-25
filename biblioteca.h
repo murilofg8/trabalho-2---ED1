3b50e4c99768fbee0476cd09e6dd72e9abc728b4
thmarcarini <thiagofabriciorm@gmail.com>
2026-06-25 18:36:03 -0300

    Update biblioteca.h with new book functions
    Added function declarations for creating and consulting books.

--- biblioteca.h
+++ biblioteca.h
@@ -18,7 +18,31 @@ typedef struct livro{
      //historico de emprestivo: usar lista ou arvore;
 }livro;
 
-int ordenaLivro(livro a, livro b);
+
+/*
+Funçao ordenaLivro: rebebe dois nós AVL e retorna um número inteiro indicando a ordem afabetica.
+Entrada: ponteiro para dois nós do tipo livro.
+Saìda: 
+    -  < 0, se livro a vier primeiro na ordem alfabética
+    -  > 0, se livro b vier primeiro na ordem alfabética
+    -    0, se forem iguais
+*/
+int ordenaLivro(livro *a, livro *b);
+
+
+/*
+Função criaLivro: rebebe os dados do dadastramento e retorna um ponteiro para no alocado dinamicamente.
+Entrada: titulo, autor, editora, edição e ano de publicação.
+Saìda: ponteiro para o nó alocado.
+*/
+livro *criaLivro(char titulo[], char autor[], char editora[], int ed, int lancamento);
+
+/*
+Função consultaLivro: rebebe o titulo e autor do livro e retorna um ponteiro para o nó do livro caso ele exista.
+Entrada: titulo e autor do livro.
+Saída: ponteiro para o nó do livro caso ele exista, ou NULL caso contrário.
+*/
+livro *consultaLivro(char titulo[], char autor[]);
 
 
 
